08d6c67c19131064fc477bfba0c51feb7a7d9f58
Asteroth2018 <99032904+Asteroth2018@users.noreply.github.com>
2022-04-29 12:55:49 +0200

    Delete sciaginj.ps1

--- sciaginj.ps1
+++
@@ -1,6 +0,0 @@
-mkdir c:\temp\testfiles
-# URL and Destination
-$url = "https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-msvc-win64.zip"
-$dest = "c:\temp\testfiles"
-# Download file
-Start-BitsTransfer -Source $url -Destination $dest 
